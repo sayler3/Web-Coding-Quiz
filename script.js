de16d52a8df7d4ae5643eea044288a39c3586e8e
Sam Ayler <sayler8701@gmail.com>
2020-10-24 15:14:38 -0700

    adding code for working timer in nav bar

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello")
+var timerCount = 100;
+var timerEl = document.querySelector("#countdown");
+
+timerEl.textContent = timerCount;
+
+var timer = window.setInterval(function() {
+    timerCount--;
+    timerEl.textContent = timerCount;
+
+    if (timerCount === 0) {
+        clearInterval(timer)
+    }
+}, 1000)
