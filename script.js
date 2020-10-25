e7eba9e8fc4f6b474fe0ad1082de2a65ac9bcab9
Sam Ayler <sayler8701@gmail.com>
2020-10-25 07:38:06 -0700

    adding comments to js

--- script.js
+++ script.js
@@ -1,80 +1,85 @@
+//creating varibles for timer
 var timerCount = 100;
 var timerEl = document.querySelector("#countdown");
 
 timerEl.textContent = timerCount;
 
-var timer = window.setInterval(function () {
-  timerCount--;
-  timerEl.textContent = timerCount;
+var timer = window.setInterval(function() {
+    timerCount--;
+    timerEl.textContent = timerCount
 
-  if (timerCount === 0) {
-    clearInterval(timer);
-  }
-}, 1000);
+    if (timerCount === 0) {
+        clearInterval(timer)
+    }
+}, 1000)
 
+//creating questions and answers
 var questions = [
-  {
-    q: "Inside which HTML element do we put the JavaScript?",
-    choices: ["<javaScript>", "<js>", "<script>"],
-    a: "C",
-  },
-  {
-    q: "Where is the correct place to insert a JavaScript?",
-    choices: ["head", "footer", "body"],
-    a: "C",
-  },
-];
+    {
+        q: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javaScript>", "<js>", "<script>"],
+        a: "C"
+    },
+    {
+        q: "Where is the correct place to insert a JavaScript?",
+        choices: ["head", "footer", "body"],
+        a: "C"
+    }]
 
-var questionsPointer = 0;
+//creating varibles for questions
+var questionsPointer = 0
 
-var question = document.querySelector("#question");
-var optionA = document.querySelector("#optionA");
-var optionB = document.querySelector("#optionB");
-var optionC = document.querySelector("#optionC");
+var question = document.querySelector("#question")
+var optionA = document.querySelector("#optionA")
+var optionB = document.querySelector("#optionB")
+var optionC = document.querySelector("#optionC")
 
+//creating function for showing questions
 function setQuestions() {
-  if (questionsPointer === questions.length) {
-    clearInterval(timer);
-    alert("you are done with " + timerCount + " time left");
-    return;
-  }
-  question.textContent = questions[questionsPointer].q;
-  optionA.textContent = questions[questionsPointer].choices[0];
-  optionB.textContent = questions[questionsPointer].choices[1];
-  optionC.textContent = questions[questionsPointer].choices[2];
+
+    if (questionsPointer === questions.length) {
+        clearInterval(timer)
+        alert("you are done with " + timerCount + " time left")
+        return
+    }
+    question.textContent = questions[questionsPointer].q
+    optionA.textContent = questions[questionsPointer].choices[0]
+    optionB.textContent = questions[questionsPointer].choices[1]
+    optionC.textContent = questions[questionsPointer].choices[2]
 }
 
-setQuestions();
+//running function for answering questions
+setQuestions() 
 
-optionA.addEventListener("click", function () {
-  if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
-    alert("That is correct");
-    questionsPointer++;
-    setQuestions();
-  } else {
-    alert("wrong");
-    timerCount -= 10;
-  }
-});
+optionA.addEventListener("click", function() {
+    if (optionA.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("That is correct")
+        questionsPointer++
+        setQuestions()
+    } else {
+        alert("wrong")
+        timerCount -= 10
+    }
+})
 
-optionB.addEventListener("click", function () {
-  if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
-    alert("This is correct");
-    questionsPointer++;
-    setQuestions();
-  } else {
-    alert("wrong");
-    timerCount -= 10;
-  }
-});
+optionB.addEventListener("click", function() {
+    if (optionB.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("This is correct")
+        questionsPointer++
+        setQuestions()
+    } else {
+        alert("wrong")
+        timerCount -= 10
+    }
+})
 
-optionC.addEventListener("click", function () {
-  if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
-    alert("this is correct");
-    questionsPointer++;
-    setQuestions();
-  } else {
-    alert("wrong");
-    timerCount -= 10;
-  }
-});
+optionC.addEventListener("click", function() {
+    if (optionC.getAttribute("data-answer") === questions[questionsPointer].a) {
+        alert("this is correct")
+        questionsPointer++
+        setQuestions()
+    } else {
+        alert("wrong")
+        timerCount -= 10
+    }
+})
